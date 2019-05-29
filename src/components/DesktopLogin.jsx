d51aa2e7d82343f1084e24cc816113d6690f09fd
Shaun Colley <swc7838@gmail.com>
2019-05-29 13:48:02 -0500

    minor change to get login mutation in

--- src/components/DesktopLogin.jsx
+++ src/components/DesktopLogin.jsx
@@ -1,7 +1,27 @@
 import React, { Component } from 'react';
 import Breakpoint from 'react-socks';
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
+
 import './Login.css';
 import { DesktopImage } from './Images';
+
+const LOGIN = gql `
+mutation login($username: String, $password: String){
+  login(username: $username, password: $password) {
+    token
+    error
+    success
+    userId
+    message
+  }
+}
+`
+
+const shaunCred = {
+  "username": "ShaunC",
+  "password": "SCwayfinder"
+}
 
 class DesktopLogin extends Component {
   render() {
@@ -14,7 +34,16 @@ class DesktopLogin extends Component {
               <input className="LoginInputField" placeholder="username" />
               <input className="LoginInputField" placeholder="password" />
               <div className="login-button">
-                <button type="button" className="LoginButton">Login</button>
+              <Mutation mutation={LOGIN} variables={shaunCred} >
+              {(login, { data, loading, error }) => {
+                if (loading) return <p>loading...</p>;
+                if (error) return <p>error...</p>;
+                if(data) {
+                  return <div>{data.login.error}</div>
+                }
+                return <button type="button" onClick={login} className="LoginButton">Login</button>
+              }}
+              </Mutation>
                 <p>Forgot Password?</p>
               </div>
             </div>
